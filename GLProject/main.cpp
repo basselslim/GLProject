335dc2c2133c4e9ed0ca0cb5809e37a690c82574
basselslim <bassel.slim99@gmail.com>
2020-05-05 17:03:50 +0200

    Initial Commit

---
+++ GLProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLProject
+//
+//  Created by Bassel Slim on 05/05/2020.
+//  Copyright © 2020 Bassel Slim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
