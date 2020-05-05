88b3607fc9be2e8f5ae78508bc4f645f1b642c1f
basselslim <bassel.slim99@gmail.com>
2020-05-05 17:09:35 +0200

    add test.cpp and test.hpp

---
+++ GLProject/test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  GLProject
+//
+//  Created by Bassel Slim on 05/05/2020.
+//  Copyright © 2020 Bassel Slim. All rights reserved.
+//
+
+#include "test.hpp"
---
+++ GLProject/test.hpp
@@ -0,0 +1,14 @@
+//
+//  test.hpp
+//  GLProject
+//
+//  Created by Bassel Slim on 05/05/2020.
+//  Copyright © 2020 Bassel Slim. All rights reserved.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
