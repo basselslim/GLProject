37c5af4b6f88719027b646a61064cf496e7c85e7
basselslim <bassel.slim99@gmail.com>
2020-05-06 10:44:42 +0200

    add test2

---
+++ GLProject/test2.cpp
@@ -0,0 +1,9 @@
+//
+//  test2.cpp
+//  GLProject
+//
+//  Created by Bassel Slim on 06/05/2020.
+//  Copyright © 2020 Bassel Slim. All rights reserved.
+//
+
+#include "test2.hpp"
---
+++ GLProject/test2.hpp
@@ -0,0 +1,14 @@
+//
+//  test2.hpp
+//  GLProject
+//
+//  Created by Bassel Slim on 06/05/2020.
+//  Copyright © 2020 Bassel Slim. All rights reserved.
+//
+
+#ifndef test2_hpp
+#define test2_hpp
+
+#include <stdio.h>
+
+#endif /* test2_hpp */
